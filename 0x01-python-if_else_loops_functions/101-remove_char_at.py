a62b7865dc9b1e9e5ab085e42ff2c4c408281951
Trendswave <paulchinedumicheal@gmail.com>
2023-10-31 19:58:12 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,10 @@
- #!/usr/bin/python3
- def remove_char_at(str, n):
-     if n < 0:
-        return(str)
-    else:
-        return(str[:n] + str[n + 1:])
+#!/usr/bin/python3
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return s  # Return the original string if n is out of bounds
+    result = ""
+    for i in range(len(s)):
+        if i != n:
+            result += s[i]
+        return result
+
