f38efe6fd0895d5b675171b23969e577baef6ecc
Trendswave <paulchinedumicheal@gmail.com>
2023-10-31 17:11:16 -0700

    edited task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+for number in range(100):
+        print("{:02d}, ".format(number), end='')
 
-in range(100):
-    print("{:02d}".format(i), end=", " if i < 99 else "\n")
+        print("100")
