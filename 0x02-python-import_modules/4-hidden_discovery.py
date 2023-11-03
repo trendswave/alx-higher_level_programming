be635fd42198d2ea99bf3b78d8708726be5b7858
Trendswave <paulchinedumicheal@gmail.com>
2023-11-02 20:28:57 -0700

    edited the hidden_4 program

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import hidden_4
-    for n in dir(hidden_4):
-        if n[0] != '_' and n[1] != '_':
-            print(n)
+    names = dir(hidden_4)
+    for n in names:
+        if n[:2] != "__":
+            print("{:s}".format(n))
