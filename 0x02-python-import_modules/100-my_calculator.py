e3e8092712101e76cb04ffa5c3ba556416fedb0b
Trendswave <paulchinedumicheal@gmail.com>
2023-11-03 00:01:54 -0700

    edited 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-f __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-    if len(argv) != 4:
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    args = len(sys.argv)
+
+    if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        a = int(argv[1])
-        b = int(arg[3])
-    if argv[2] == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif argv[2] == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif argv[2] == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif argv[2] == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    else:
+        sys.exit(1)
+        # Arguments initiated
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+        #operational performance
+        result = add(a, b)
+        elif operator == '-':
+        result = sub(a, b)
+        elif operator == '*':
+        result = mul(a, b)
+        elif operator == '/':
+        result = div(a, b)
+        else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
+        # sequencial result output 
+        print("{} {} {} = {}".format(a, operator, b, result))
