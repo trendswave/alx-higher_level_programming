2f585a23a21ae68c1409a6511cbea08cdfd4708e
trendswave <paulchinedumicheal@gmail.com>
2023-11-28 22:51:57 +0100

    tasks added

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(tmp=[]):
+    tmp += ['Holberton']
+    return ', '.join(tmp)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' Locked class module'''
+
+
+class LockedClass:
+    '''Object that prevents dynamics attributes'''
+
+    __slots__ = ['first_name']
