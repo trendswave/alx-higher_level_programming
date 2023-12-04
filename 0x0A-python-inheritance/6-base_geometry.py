a5dc50ab7eabd2b75b3afefe0313c9a5ae439cfd
trendswave <paulchinedumicheal@gmail.com>
2023-12-04 21:52:50 +0100

    A class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Class BaseGeometry'''
+
+    def area(self):
+        '''Method not implemented'''
+        raise Exception('area() is not implemented')
