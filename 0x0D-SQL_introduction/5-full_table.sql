2b68c416bdd83e0b0a6f9d4b831ce0ff45b285da
trendswave <paulchinedumicheal@gmail.com>
2024-01-16 17:14:53 +0100

    Updated task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,7 @@
---A script that prints full description of table; without using 'DECSCRIBE or EXPLAIN statement [tableName]'
+-- print full description of table; without using 'DECSCRIBE [tableName]'
+--
+-- Table   Create Table
+-- first_table CREATE TABLE `first_table` (\n  `id` int(11) DEFAULT NULL,
+-- \n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
 SHOW CREATE TABLE first_table;
